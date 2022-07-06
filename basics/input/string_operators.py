c7049ad8298c35cff3e712fd2942b9651d15c274
ViHri <108287937+V1Hr1@users.noreply.github.com>
2022-07-06 21:54:07 +0000

    W2

---
+++ basics/input/string_operators.py
@@ -0,0 +1,7 @@
+lives = int(input("Please enter the number of lives: "))
+energy = int(input("Please enter the energy level: "))
+shield = int(input("Please enter the shield level: "))
+print("Health has been set")
+print("Lives: ", "\u2665"*lives)
+print("Energy: ", "\u2666"*energy)
+print("Shields: ", "\u2720"*shield)
